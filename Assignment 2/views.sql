a1d5160f7e81fadf60037042cfec7903823da15f
stuartitm <ddggstuart@gmail.com>
2018-11-12 17:00:10 -0500

    views and stored procedures finished and working

--- Assignment 2/views.sql
+++ Assignment 2/views.sql
@@ -9,7 +9,11 @@ ALTER TABLE trips
 UPDATE TRIPS SET city_id=1;
 
 --creation of table "companies" to save the companies that can support the bussines accounts 
-CREATE TABLE companies (id number, name VARCHAR2(256));
+CREATE SEQUENCE companies_seq START WITH 1;
+CREATE TABLE companies (id number primary key, name VARCHAR2(256));
+INSERT INTO companies (id, name) VALUES (companies_seq.NEXTVAL, 'Capgemini');
+UPDATE user_payment_methods SET company_id=1, business_account='t' where user_id = 3;
+
 
 --add fields "business_account" and company_id to storage if the payment method is supported by a company and the reference to the company in such case
 ALTER TABLE user_payment_methods ADD business_account CHAR(1);
@@ -20,6 +24,12 @@ ALTER TABLE user_payment_methods
         REFERENCES companies ( id );
 
 
+--Add field to trip_statuses to be able to know what of the statuses is the successful one without to hardcode strings
+ALTER TABLE trip_statuses ADD successful_trip CHAR(1);
+UPDATE trip_statuses SET successful_trip = 'f';
+UPDATE trip_statuses SET successful_trip = 't' WHERE name = 'Successful';
+
+
 --1
 CREATE OR REPLACE VIEW MEDIOS_PAGO_CLIENTES AS
 SELECT 
@@ -27,7 +37,7 @@ SELECT
     u.name || ' ' || u.last_name as NOMBRE_CLIENTE,
     pm.id as MEDIO_PAGO_ID,
     pm.name as TIPO,
-    upm.card_number,
+    upm.card_number as DETALLES_MEDIO_PAGO,
     case when upm.business_account = 'f' or upm.business_account is null then 'FALSO' else 'VERDADERO' end as EMPRESARIAL,
     c.name as NOMBRE_EMPRESA    
 FROM users u
@@ -75,18 +85,68 @@ INSERT INTO cities (id, name, country_id, base_value, value_per_min, value_per_k
 CREATE OR REPLACE FUNCTION VALOR_DISTANCIA(km IN FLOAT, city_name IN VARCHAR2) 
    RETURN FLOAT
 IS
+    negative_distance_ex EXCEPTION;
+    city_not_found_ex EXCEPTION;
+    city_count INT;
     total_value FLOAT;
 BEGIN
+    IF(km<0) THEN
+        RAISE negative_distance_ex;
+    END IF;
+    SELECT COUNT(*) INTO city_count FROM cities WHERE NAME = city_name;
+    IF(city_count=0) THEN
+        RAISE city_not_found_ex;
+    END IF;
     SELECT value_per_km * km INTO total_value FROM cities WHERE NAME = city_name;
     RETURN total_value;
 END VALOR_DISTANCIA;
 
 --6
-CREATE OR REPLACE FUNCTION VALOR_TIEMPO(min IN FLOAT, city_name IN VARCHAR2) 
+CREATE OR REPLACE FUNCTION VALOR_TIEMPO(minutes IN FLOAT, city_name IN VARCHAR2) 
    RETURN FLOAT
 IS
+    negative_time_ex EXCEPTION;
+    city_not_found_ex EXCEPTION;
+    city_count INT;
     total_value FLOAT;
 BEGIN
-    SELECT value_per_min * min INTO total_value FROM cities WHERE NAME = city_name;
+    IF(minutes<0) THEN
+        RAISE negative_time_ex;
+    END IF;
+    SELECT COUNT(*) INTO city_count FROM cities WHERE NAME = city_name;
+    IF(city_count=0) THEN
+        RAISE city_not_found_ex;
+    END IF;
+    SELECT value_per_min * minutes INTO total_value FROM cities WHERE NAME = city_name;
     RETURN total_value;
 END VALOR_TIEMPO;
+
+CREATE OR REPLACE PROCEDURE CALCULAR_TARIFA (trip_id_param IN INT)
+AS
+BEGIN
+    MERGE INTO trip_user_bills tub
+    USING (
+        SELECT tu.id as trip_user_id, tu.trip_id, t.distance_covered, t.start_date, t.end_date, c.base_value, c.name AS city_name, ts.successful_trip FROM trips t
+            INNER JOIN cities c ON c.id = t.city_id
+            INNER JOIN trip_users tu ON t.id = tu.trip_id
+            INNER JOIN trip_statuses ts ON t.trip_status_id = ts.id
+            WHERE t.id = trip_id_param
+    ) jtu
+    ON (jtu.trip_user_id = tub.trip_user_id)
+    WHEN MATCHED THEN UPDATE SET tub.total = CASE NVL(jtu.successful_trip,'f') WHEN 't' THEN
+            ((jtu.base_value + 
+            VALOR_TIEMPO(round((CAST(jtu.end_date AS DATE) - CAST(jtu.start_date AS DATE)) * (60 * 24),2), jtu.city_name) +
+            VALOR_DISTANCIA(jtu.distance_covered, jtu.city_name)) / (SELECT COUNT(*) FROM trip_users itu WHERE itu.trip_id=trip_id_param))
+            + tub.surcharges + tub.tip
+        ELSE 
+            0
+        END
+    WHERE jtu.trip_id=trip_id_param;
+EXCEPTION
+    WHEN others THEN
+        MERGE INTO trip_user_bills tub
+        USING trip_users tu
+        ON (tu.id = tub.trip_user_id)
+        WHEN MATCHED THEN UPDATE SET tub.total = 0
+        WHERE tu.trip_id = trip_id_param;
+END;
